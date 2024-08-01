3d65514e2e97d67e051e0b89360fb5c2d528f1ce
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2024-08-01 09:24:25 +0000

    Pending changes exported from your codespace

---
+++ frontend/src/Resources.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Resources = () => {
+  return (
+    <div className='text-5xl font-semibold mb-20 relative top-8 right-60'>Resources</div>
+  )
+}
+
+export default Resources
