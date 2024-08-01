17b9b741eedf939b586ab8d6a40876a0aa06185f
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2024-08-01 09:14:29 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx
    Untracked files:
    	src/Resources.css
    	src/Resources.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Banner from './Components/Banner'
 import "./App.css";
+import Resources from './Resources';
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
+        <Resources/>
         <Banner />
       </div>
     </>
